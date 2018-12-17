e68269d88557f03f1c225019762cf56aea0e084d
Mark Florence <mflo999@gmail.com>
2018-12-17 05:05:43 -0500

    Cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
 /**
+ * @see https://stackoverflow.com/questions/39392853
+ */
+
+export interface Class {
+  new(...args: any[]): any;
+}
+
+/**
  * KV pairs eg: request headers
  */
 
@@ -6,4 +14,3 @@ export interface Map<T> {
   [k: string]: T;
 }
 
-export interface Class { new(...args: any[]): any; }
