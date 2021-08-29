b543fe49e74c2c91bb70ef43aa11594ae1cc0b4b
Mark Florence <mflo999@gmail.com>
2021-08-29 04:04:58 -0400

    Prepare new version for release

---
+++ bin/deploy-npm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+version=${1:-patch}
+message=${2:-"Prepare new version for release"}
+
+npm run build
+
+git add . *
+git commit -m "$message"
+
+npm version $version
+
+git push origin main
+
+npm publish
